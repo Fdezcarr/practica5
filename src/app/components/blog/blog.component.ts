d13dd6972153dfa846e7d5515e5922511337fdb5
Adrián Fernández Carrasco <fernandezcarrascoadrian@gmail.com>
2024-07-07 19:41:58 +0200

    Update blog.component.ts

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Post } from '../../interfaces/post.interface';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { log } from 'console';
 
 @Component({
   selector: 'app-blog',
@@ -12,8 +11,6 @@ import { log } from 'console';
   styleUrl: './blog.component.css',
 })
 export class BlogComponent {
-  //formulario
-
   newpost: Post = { titulo: '', imagen: '', fecha: '', texto: '' };
 
   getDataForm() {
@@ -23,16 +20,16 @@ export class BlogComponent {
 
   arrPost: Post[] = [
     {
-      titulo: 'en un lugar',
+      titulo: 'Iphone16',
       imagen:
-        'https://upload.wikimedia.org/wikipedia/commons/f/fa/Paisaje%2C_provincia_de_Ciudad_Real_%283742104133%29.jpg',
+        'https://es.digitaltrends.com/wp-content/uploads/2024/05/iPhone-16-Pro-Max.jpeg?fit=1132%2C743&p=1',
       fecha: '24/07/2024',
       texto: 'jjjjjjsssss',
     },
     {
-      titulo: 'en un lugarddddd',
+      titulo: 'Macbook',
       imagen:
-        'https://upload.wikimedia.org/wikipedia/commons/f/fa/Paisaje%2C_provincia_de_Ciudad_Real_%283742104133%29.jpg',
+        'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbookair-og-202402?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1707414684423',
       fecha: '24/07/2024',
       texto: 'jjjjjjsssss',
     },
